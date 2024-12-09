72e6bb9179517c235c342887fb66679587ac7279
ASUS\William2_Lai <William2_Lai@asus.com>
2024-12-09 21:10:30 +0800

    0~255

--- OvmfPkg/HelloWorld/HelloWorld.c
+++ OvmfPkg/HelloWorld/HelloWorld.c
@@ -9,6 +9,8 @@
 #include <IndustryStandard/Pci.h>
 #include <Protocol/PciIo.h>
 
+#include <Library/IoLib.h>
+
 
 // HelloWorldEntryPoint()
 EFI_STATUS
@@ -39,7 +41,46 @@ HelloWorldEntryPoint(
     Offset = 0;
     Addr = 0;
 
+    Print(L"Address|Reserved|BusNum|Device|Function|Register");
 
-    // Return EFI_SUCCESS
+    for(Offset;Offset <= 255 ; Offset = Offset + 4)
+    {
+        Addr = PCIBaseAddr + Offset;
+        Print(L"%x ", MmioRead32(Addr));
+        if(Offset % 15 == 0 ) Print(L"\n");
+        // Print(L"1");
+    }
+
+    Print(L"---------------------------------------------------\n");
+
+    // for(Bus = 0 ; Bus <= 0xFF ; Bus++)
+    // {
+    //     for(Device = 0 ; Device <= 0x1F ; Device++)
+    //     {
+    //         for(Function = 0 ; Function <= 0x7 ; Function++)
+    //         {
+    //             Addr = PCIBaseAddr + (Bus << 16) + (Device << 11) + (Function << 8);
+    //             Print(L"Data at address 0x%08x: \n", Addr);
+
+    //             // Print(L"DeviceID: %d ,VendorID: %d ,FunctionID: %d ,");
+    //             // Print(L"1");
+    //         }
+    //     }
+    // }
+
+    Print(L"---------------------------------------------------\n");
+    Offset = 0;
+
+    // while(Offset <= 0x100)
+    // {
+    //     Addr = PCIBaseAddr + Offset;
+    //     Print(L"0x%x ", MmioRead32(Addr));
+    //     if(Offset%15 == 0)
+    //     {
+    //         Print(L"\n");
+    //     }
+    //     Offset++;
+    // }
+
     return Status;
 }
